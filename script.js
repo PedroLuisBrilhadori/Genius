72b1e087b9dad87e4a740fe0c82fb39ec6768361
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2021-06-14 14:16:01 -0300

    tudo tem um recomeço

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 jogo = {
     computadorValores: [],
     jogadorValores: [],
+    tamanhoMaximo = 4,
+    estagioDaCombinacao = 1,
+    combinacaoMaxima = 15,
+
+
+    interface = {},
+
+    carregar() {},
     
+    comecar() {
+        jogo.computadorValores = jogo.criarCombinacao()
+        jogo.estagioDaCombinacao = 1;
+        jogo.jogadorValores = [];
+    },
+
+    criarCombinacao(){
+        let novaCombinacao = []
+        for (let i = 0; i < jogo.combinacaoMaxima; i++){
+            const position = Math.floor((Math.random() * jogo.tamanhoMaximo) + 1)
+            novaCombinacao.push(position - 1)
+        }
+        return novaCombinacao;
+    }
+
+
 }
